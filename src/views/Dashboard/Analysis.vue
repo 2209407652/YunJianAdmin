add245086b795ad13a2e9bc2a2dc035fc9d466c1
Sssungl <10329526+sssungl@user.noreply.gitee.com>
2025-01-15 20:34:28 +0800

    f

---
+++ src/views/Dashboard/Analysis.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ElRow, ElCol, ElCard } from 'element-plus'
+</script>
+
+<template>
+  <ElRow>
+    <ElCol>
+      <ElCard> 白色背景 </ElCard>
+    </ElCol>
+  </ElRow>
+</template>
